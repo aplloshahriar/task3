e147c5195064e51e15544150257eea21f8b7ef91
aplloshahriar <aaronshahriar.as@gmail.com>
2024-05-28 22:07:19 +0600

    first commit

---
+++ toggle.js
@@ -0,0 +1,11 @@
+// toggle.js
+
+function toggleMenu() {
+  const dropdownMenu = document.getElementById("dropdown-menu");
+  dropdownMenu.classList.toggle("hidden");
+}
+
+function toggleMobileMenu() {
+  const mobileMenu = document.getElementById("mobile-menu");
+  mobileMenu.classList.toggle("hidden");
+}
